1d0e9edc6dae24cd5c5fd04d94b4e563086ae338
TAMIZHARASU P <161800487+GitTamizh@users.noreply.github.com>
2024-07-10 19:46:33 +0530

    app.js
    updated the code using selectors to  open and close the answers page.

--- Javascript Projects/07- Questions/Questions/app.js
+++ Javascript Projects/07- Questions/Questions/app.js
@@ -1,11 +1,36 @@
 //using selectors inside the element
+//using selectors inside the element
+const questions = document.querySelectorAll(".question");
+
+questions.forEach(function(question){
+    // selecting all the buttons
+    const button = question.querySelector(".question-btn");
+
+    //  adding eventlister for open the show-text in the QA.
+    button.addEventListener('click', function(){
+
+        // adding foreach function for removing the answer (show-text) box if it opened.
+        questions.forEach(function(qns){
+            if(qns !== question){
+                qns.classList.remove('show-text');
+            }
+            
+        });
+
+        question.classList.toggle('show-text');
+    });
+    
+});
+
+
+
 // traversing the dom
 
-const buttons = document.querySelectorAll(".question-btn");
+// const buttons = document.querySelectorAll(".question-btn");
 
-buttons.forEach(function(btn){
-    btn.addEventListener('click', function(e){
-        const text = e.currentTarget.parentElement.parentElement;
-        text.classList.toggle('show-text');
-    });
-});
+// buttons.forEach(function(btn){
+//     btn.addEventListener('click', function(e){
+//         const text = e.currentTarget.parentElement.parentElement;
+//         text.classList.toggle('show-text');
+//     });
+// });
