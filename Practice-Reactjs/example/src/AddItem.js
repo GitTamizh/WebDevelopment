5661bdf853f6f40f1f198966545b8dfdedd87069
TAMIZHARASU P <tamizharasu.p2@outlook.com>
2024-07-22 20:26:03 +0530

    added useref and useeffect components in the react app

--- Practice-Reactjs/example/src/AddItem.js
+++ Practice-Reactjs/example/src/AddItem.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import { FaPlus } from 'react-icons/fa'
+import { useRef } from 'react'
 const AddItem = ({newItem, setNewItem, handleSubmit}) => {
+    const inputRef = useRef()
     return (
         <form className='addForm' onSubmit={handleSubmit}>
             <label htmlFor="addItem">Add Item</label>
             <input 
             autoFocus
+            ref={inputRef}
             id="addItem"
             type="text" 
             placeholder="Add Item"
@@ -15,7 +18,8 @@ const AddItem = ({newItem, setNewItem, handleSubmit}) => {
             
             <button 
             type="submit"
-            aria-label="Add Item">
+            aria-label="Add Item"
+            onClick={() => inputRef.current.focus()}>
                 <FaPlus />
             </button>
         </form>
--- Practice-Reactjs/example/src/App.js
+++ Practice-Reactjs/example/src/App.js
@@ -2,14 +2,20 @@ import Header from './Header';
 import Content from './Content';
 import Footer from './Footer';
 import { useState } from 'react';
+import { useEffect } from 'react';
 import AddItem from './AddItem';
 import SearchItem from './SearchItem';
 
 function App() {
-  const [items, setItems] = useState(JSON.parse(localStorage.getItem("todo_list")))
+  const [items, setItems] = useState([])
 
     const [newItem, setNewItem] = useState('')
     const [search, setSearch] = useState('')
+
+    useEffect(( ) => {
+        JSON.parse(localStorage.getItem("todo_list"))
+    }, [])
+    
     const addItem = (item) => {
       const id = items.length ? items[items.length - 1].id + 1 : 1 ;
       const addNewItem = {id,checked: false, item}
@@ -35,7 +41,7 @@ function App() {
     const handleSubmit = (e) => {
       e.preventDefault()
       if(!newItem) return;
-      console.log("submitted");
+      // console.log("submitted");
       addItem(newItem)
       setNewItem('')
     }
